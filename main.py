a7f1c62f34a2911e0a13f9fdf6aaf3d0d63e804c
doanvtamhuynh <huynhdoan.2132@gmail.com>
2024-09-25 11:46:07 +0700

    feat: multithreaded

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import Logic.ReadFile as ReadFile
 import Logic.Gmail_Services as Gmail
 from Logic.WriteFile import WriteInfo
 import os
+import threading
+import asyncio
 
 ACCOUNT = ReadFile.GetUsernamePassword()
 ACCOUNT_USERNAME = ACCOUNT[0]
@@ -20,67 +22,76 @@ root_dir = os.getcwd()
 src = rf"{root_dir}\ListFile"
 
 listEmail = ReadFile.GetListEmail(rf"{src}\listEmail.txt")
-if listEmail is not None:
-    email = listEmail[0]
 
 listProfile = ReadFile.GetListProfile(rf"{src}\listProfile.txt")
-if listProfile is not None:
-    profile = listProfile[0]
 
 listWordTranslate = ["hello", "thanks", "good"]
 listWordAlert = ["usa", "car", "football"]
 
-print(email)
-print(profile)
+async def task(email: NewEmail, profile: NewProfile):
+    print(email)
+    print(profile)
 
-driver = MultiLogin.Start(ACCOUNT_USERNAME, ACCOUNT_PASSWORD, profile)
-if driver is not None:
 
-    resultLogin = Google.GG_Login(driver, email)
-    time.sleep(1)
+    driver = MultiLogin.Start(ACCOUNT_USERNAME, ACCOUNT_PASSWORD, profile)
+    if driver is not None:
 
-    if resultLogin is True:
+        resultLogin = Google.GG_Login(driver, email)
+        await asyncio.sleep(2)
 
-        Google.GG_Translate(driver, listWordTranslate)
-        time.sleep(2)
-        Google.GG_ALert(driver, listWordAlert)
-        time.sleep(2)
+        if resultLogin is True:
 
-        #Sign In Other Website
-        OtherWebsite.Website_Youtube(driver)
-        time.sleep(2)
-        OtherWebsite.Website_TLDR(driver, email)
-        time.sleep(2)
-        OtherWebsite.Website_Envalior(driver, email)
-        time.sleep(2)
-        OtherWebsite.Website_InfoQ(driver, email)
-        time.sleep(2)
-        OtherWebsite.Website_Dictionary(driver, email)
-        time.sleep(2)
-        OtherWebsite.Website_Quora(driver)
-        time.sleep(2)
-        OtherWebsite.Website_ITViec(driver)
-        time.sleep(2)
-        OtherWebsite.Website_Foxnews(driver, email)
-        time.sleep(2)
+            Google.GG_Translate(driver, listWordTranslate)
+            await asyncio.sleep(2)
+            Google.GG_ALert(driver, listWordAlert)
+            await asyncio.sleep(2)
 
-        #Read Mail
-        Gmail.ReadMail_TLDR(driver)
-        time.sleep(2)
-        Gmail.ReadMail_Envalior(driver)
-        time.sleep(2)
-        Gmail.ReadMail_InfoQ(driver)
+            #Sign In Other Website
+            OtherWebsite.Website_Youtube(driver)
+            await asyncio.sleep(2)
+            OtherWebsite.Website_TLDR(driver, email)
+            await asyncio.sleep(2)
+            OtherWebsite.Website_Envalior(driver, email)
+            await asyncio.sleep(2)
+            OtherWebsite.Website_InfoQ(driver, email)
+            await asyncio.sleep(2)
+            OtherWebsite.Website_Dictionary(driver, email)
+            await asyncio.sleep(2)
+            OtherWebsite.Website_Quora(driver)
+            await asyncio.sleep(2)
+            OtherWebsite.Website_ITViec(driver)
+            await asyncio.sleep(2)
+            OtherWebsite.Website_Foxnews(driver, email)
+            await asyncio.sleep(2)
 
-        OtherWebsite.Website_Batdongsan(driver)
-        time.sleep(2)
+            #Read Mail
+            Gmail.ReadMail_TLDR(driver)
+            await asyncio.sleep(2)
+            Gmail.ReadMail_Envalior(driver)
+            await asyncio.sleep(2)
+            Gmail.ReadMail_InfoQ(driver)
 
-        # Create info
-        newInfoAccount = ReadFile.GetInfoAccount()
-        print(newInfoAccount)
+            OtherWebsite.Website_Batdongsan(driver)
+            await asyncio.sleep(2)
 
-        # Google Services
-        Google.Change_Info(driver, newInfoAccount)
-        WriteInfo(email, newInfoAccount, rf"{src}\newEmail.txt")
+            # Create info
+            newInfoAccount = ReadFile.GetInfoAccount()
+            print(newInfoAccount)
 
-    time.sleep(5)
-    MultiLogin.Stop(profile)
+            # Google Services
+            Google.Change_Info(driver, newInfoAccount)
+            WriteInfo(email, newInfoAccount, rf"{src}\newEmail.txt")
+
+        await asyncio.sleep(5)
+        MultiLogin.Stop(profile)
+
+
+async def main():
+    tasks = []
+    for i in range(len(listProfile) - 1):
+        tasks.append(task(listEmail[i], listProfile[i]))
+
+    await asyncio.gather(*tasks)
+
+asyncio.run(main())
+
